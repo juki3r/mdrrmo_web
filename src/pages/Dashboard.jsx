b5fe80c7cb563f5a06dea46e700ee6632af936e8
juki3r <deepong25@gmail.com>
2026-06-03 15:06:03 +0800

    aa

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -35,6 +35,19 @@ import {
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 export default function Dashboard() {
   const token = localStorage.getItem("token");
 
